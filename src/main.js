f2a38c8befa8b0b9945f2fdeb2c0ad6a1bd44392
TwlSweet <530449639@qq.com>
2019-03-26 17:07:23 +0800

    注册表单验证

--- src/main.js
+++ src/main.js
@@ -2,12 +2,16 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// 如果引入的是 index.js，可以使用下面的简写，等价于 import router from './router/index.js'
+import router from './router'
+import './directives'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { App },
   template: '<App/>'
 })
